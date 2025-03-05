10b83d13d84b4ca9c859274e1d30131c8fadaa02
songcw8 <ben990158@gmail.com>
2025-03-05 10:23:24 +0900

    add

--- src/Solution01.java
+++ src/Solution01.java
@@ -1,3 +1,20 @@
-public class Solution01 {
+import java.util.logging.Logger;
 
+public class Solution01 {
+    public static void main(String[] args) {
+        ICalculator calculator = new Calculator();
+        Logger logger = Logger.getLogger("calculator");
+        logger.info("%d".formatted(calculator.add(1, 2)));
+    }
+}
+
+class Calculator implements ICalculator {
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+interface ICalculator{
+    int add(int a, int b);
 }
